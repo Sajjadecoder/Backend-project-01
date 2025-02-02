8c2da60b4b8f2dcc037030832577bf2d00817fe3
Sajjadecoder <sajjadahmed102005@gmail.com>
2025-02-02 18:50:34 +0500

    added files apiError,apiResponse & asyncHandler

--- src/index.js
+++ src/index.js
@@ -2,26 +2,37 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
 dotenv.config({
     path: './.env'
 });
 
 const app = express();
 
+app.use(cors(
+    {
+        origin:'*',
+        credentials: true
+    }
+))
+app.use(express.json({limit: '16kb'})); // will accept only json data upto 16kb
+app.use(express.urlencoded());
+app.use(express.static('public'));// allow temporary storage of files in public folder
+app.use(cookieParser());
 (async()=>{
     try {
         console.log('' + process.env.MONGODB_URL)
         const connectDB = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        app.on('error', (error) => {
-            console.log('ERROR: ',error)
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log('Server is running on port: ', process.env.PORT)
-        })
 
         console.log(`Connected to database: ${connectDB.connection.host}`)
     } catch (error) {
         console.error('Connection failed ',error)
     }
+}).then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log('Server is running on port 5000')
+    })
 })()
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+//method1
+// const asyncHandler = (fn) => async(req, res, next) => {
+    //     try {
+        //         await fn(req, res, next)
+        //     } catch (error) {
+            //         res.status(error.status || 500).json({ message: error.message || 'Something went wrong' })
+            //     }
+            
+            // }
+            
+//method2
+const asyncHandler = (func) =>{
+    (req,res,next) =>{
+        Promise.resolve(func(req,res,next)).catch((err)=>next(err))
+    
+    }
+}
+
+export {asyncHandler}
